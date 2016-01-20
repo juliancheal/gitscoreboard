348054dc2f6b5867f527497da57d956a3f63cf4a
Julian Cheal <jcheal@redhat.com>
2016-01-20 20:21:20 +0000

    Initial working version of Scoreboard

--- scoreboard.rb
+++ scoreboard.rb
@@ -1,6 +1,24 @@
+require 'octokit'
+require 'ostruct'
 require 'dotenv'
 Dotenv.load
 
-client = Octokit::Client.new \
-  client_id: ENV['GH_CLIENT_ID'],
-  client_secret: ENV['GH_CLIENT_SECRET']
+# Scoreboard downloads a github repos statistics
+class Scoreboard
+  def initialize
+    @client = Octokit::Client.new \
+      client_id: ENV['GH_CLIENT_ID'],
+      client_secret: ENV['GH_CLIENT_SECRET']
+    @users = []
+    @stats = client.contributors_stats(ENV['GH_REPO'])
+  end
+
+  def grab_statistics
+    @stats.each do |stat|
+      user = OpenStruct.new(stat[:author].to_hash)
+      user.total_commits = stat[:total]
+      user.weeks = stat[:weeks]
+      @users << user
+    end
+  end
+end
